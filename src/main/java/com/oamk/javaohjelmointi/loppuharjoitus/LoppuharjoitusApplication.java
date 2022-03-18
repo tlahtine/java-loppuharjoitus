ac68475a4cc791564dcb376099a3962bfeacf972
Teemu Lahtinen <tlahtine@gmail.com>
2022-03-18 10:03:26 +0200

    project created

---
+++ src/main/java/com/oamk/javaohjelmointi/loppuharjoitus/LoppuharjoitusApplication.java
@@ -0,0 +1,13 @@
+package com.oamk.javaohjelmointi.loppuharjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoppuharjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoppuharjoitusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oamk/javaohjelmointi/loppuharjoitus/LoppuharjoitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oamk.javaohjelmointi.loppuharjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoppuharjoitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
